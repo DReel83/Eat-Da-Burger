d308aaabced7000af740ed0fc1cb5f8aa483300d
David Reel <davidpreel@gmail.com>
2016-09-23 19:47:42 -0400

    added seeds

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO burgers (burger_name) VALUES ('Philly Cheese Steak Burger');
+INSERT INTO burgers (burger_name) VALUES ('Juicy Lucy');
+INSERT INTO burgers (burger_name) VALUES ('Black and Blue');
+INSERT INTO burgers (burger_name) VALUES ('Paddy Melt');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Egg and Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Kobe Burger');
+INSERT INTO burgers (burger_name) VALUES ('Waygu Burger');
+INSERT INTO burgers (burger_name) VALUES ('50/50 Burger');
+INSERT INTO burgers (burger_name) VALUES ('Wahl-Burger');
+INSERT INTO burgers (burger_name) VALUES ('Plain Jane');
